d3e8cf366bc0cc0bc2f8b1cdcc3cbdd6d00b2772
blaire-pi <56533266+blaire-pi@users.noreply.github.com>
2022-11-30 13:52:32 +0900

    addPlaceSound

--- PlaceSound/PlaceSound/AddPlaceSound.swift
+++ PlaceSound/PlaceSound/AddPlaceSound.swift
@@ -8,8 +8,69 @@
 import SwiftUI
 
 struct AddPlaceSound: View {
+    //    @State private var isClicked: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        ZStack {
+            VStack(alignment: .leading, spacing: 20) {
+                Spacer()
+                Group {
+                    Text("Place Sound 추가하기")
+                        .fontWeight(.bold)
+                        .padding(.leading, 15.0)
+                    Divider()
+                    //                        .padding(0)
+                }
+                Spacer()
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        Text("place")
+                            .padding(.leading, 15.0)
+                        VStack {
+                            HStack {
+                                Text("장소를 입력해주세요")
+                                    .padding(.leading, 15.0)
+                                Spacer()
+                            }
+                            Divider()
+                        }
+                        .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 20.0)
+                    
+                    VStack {
+                        HStack {
+                            Text("Sound")
+                            Spacer()
+                        }
+                            .padding(.leading, 15.0)
+                        SearchBar()
+                            .padding()
+                        AddedListView()
+                    }
+                    //                .padding()
+                    
+                    Spacer()
+                    VStack {
+                        Spacer()
+                        Button {
+                            //                isClicked = true
+                        } label: {
+                            ZStack {
+                                Rectangle()
+                                    .cornerRadius(40)
+                                    .frame(width: 310, height: 50)
+                                Text("저장하기")
+                                    .foregroundColor(.white)
+                            }
+                        }
+                        .padding()
+                    }
+                }
+                
+            }
+         
+//        }
     }
 }
 
