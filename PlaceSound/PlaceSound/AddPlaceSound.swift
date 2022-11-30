319cc62b932bc4e160fd202acb753f51bb9e3293
Bomi Kim <71758542+bomyuniverse@users.noreply.github.com>
2022-11-30 11:46:00 +0900

    Add: View Files

---
+++ PlaceSound/PlaceSound/AddPlaceSound.swift
@@ -0,0 +1,20 @@
+//
+//  AddPlaceSound.swift
+//  PlaceSound
+//
+//  Created by 김보미 on 2022/11/30.
+//
+
+import SwiftUI
+
+struct AddPlaceSound: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddPlaceSound_Previews: PreviewProvider {
+    static var previews: some View {
+        AddPlaceSound()
+    }
+}
---
+++ PlaceSound/PlaceSound/SearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar.swift
+//  PlaceSound
+//
+//  Created by 김보미 on 2022/11/30.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBar()
+    }
+}
